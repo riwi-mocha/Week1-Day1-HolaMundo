66c8c53f667660a6b05ed3ae392671d5af2afced
JuanAndresVallejo <jvallejo.devops@gmail.com>
2025-09-08 21:32:13 -0500

    feat: Creacion de primer archivo Java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+
+    //Name in variable
+    public static void main(String[] args) {
+        String name = "Juan Andres";
+        System.out.println("Hello World " +name + "!");
+    }
+}
