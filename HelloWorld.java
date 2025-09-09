7049150fa106c6cc80182dc7b803de1bb8844a82
Johan192004 <johanruma@gmail.com>
2025-09-08 21:30:39 -0500

    feat: add HelloWorld functionality

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+
+    static String nombre = "Johan";
+
+    public static void main(String[] args){
+        String nombre2 = "Johan Ramirez";
+        System.out.printf("Hello world %s, How are you?\n",nombre);
+        System.out.println("Hello world, " + nombre2);
+
+    }
+
+}
